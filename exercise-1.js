05eb1a9939ab29f5a9f9e66d5f751271a57ca8b9
alantheandroid <alessio.ilardi@gmail.com>
2022-05-29 22:58:14 +0200

    feat: add getter methods

--- exercise-1.js
+++ exercise-1.js
@@ -1,6 +1,18 @@
-const person = {
-  // ...
+const john = {
+  firstName: "John",
+  lastName: "Doe",
+  get fullName () {
+    return `${john.firstName} ${john.lastName}`
+  }
 }
 
-console.log(john.fullName()); // John Doe
-console.log(simon.fullName()); // Simon Collins
+const simon = {
+  firstName: "Simon",
+  lastName: "Collins",
+  get fullName () {
+    return `${simon.firstName} ${simon.lastName}`
+  }
+}
+
+console.log(john.fullName); // John Doe
+console.log(simon.fullName); // Simon Collins
